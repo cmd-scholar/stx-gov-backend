4616977786b919da8c70b8ac78333a72f00b31b4
cmd-scholar <adeyemiwarith6@gmail.com>
2024-09-18 17:09:18 +0100

    feat: added create dao endpoint to the stacks gov backendfeat: fixed get endpoint

--- app/daos/crud.py
+++ app/daos/crud.py
@@ -1,15 +1,24 @@
-from sqlmodel import select
+from sqlalchemy import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-from app.daos.models import DaoRead, Dao
+from app.daos.models import DaoRead, Dao, DaoCreate
 
 
 class DaosCRUD:
     def __init__(self, session: AsyncSession):
         self.session = session
 
+    async def create(self, data: DaoCreate) -> DaoRead:
+        values = data.model_dump()
+        dao = Dao(**values)
+        self.session.add(dao)
+        await self.session.commit()
+        await self.session.refresh(dao)
+
+        return dao
+
     async def get_all(self) -> list[DaoRead]:
         statement = select(Dao)
         results = await self.session.execute(statement)
         daos = results.scalars().all()
-
         return daos
+
